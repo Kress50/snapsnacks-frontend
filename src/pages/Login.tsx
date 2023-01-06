46de5f11360d319fca6b09049b62690c4fd85ebf
Kress50 <kondalovi@gmail.com>
2023-01-07 01:44:03 +0400

    added signUp page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,6 +8,8 @@ import {
 import testLogo from "../images/ruby.svg";
 import { Button } from "../components/UI/Button";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
+import { IsLoggedInVar } from "../apollo";
 
 const LOGIN_MUTATION = gql`
   mutation LoginMutation($loginAccountInput: LoginAccountInput!) {
@@ -37,6 +39,7 @@ export default function Login() {
     } = data;
     if (ok) {
       console.log(token);
+      IsLoggedInVar(true);
     }
   };
   const [loginMutation, { data: loginMutationResult, loading }] = useMutation<
@@ -60,55 +63,69 @@ export default function Login() {
   }
 
   return (
-    <div className="flex h-screen flex-col items-center">
-      <div className="mt-12 flex w-full max-w-screen-sm flex-col items-center px-5 md:mt-36">
-        <img src={testLogo} alt="test-logo" className="w-20" />
-        <h3 className="mt-8 w-full text-3xl font-semibold">Welcome back!</h3>
-        <p className="text-md mt-4 w-full">
-          Login using your account credentials
-        </p>
-        <form
-          onSubmit={handleSubmit(onSubmitHandler)}
-          className="mt-4 mb-5 flex w-full flex-col"
-        >
-          <div className="mb-4 flex flex-col">
-            <input
-              className="input"
-              {...register("email", { required: "Email is required" })}
-              placeholder="email"
-              type="email"
-              required
-            />
-            {errors.email?.message && (
-              <FormError errorMessage={errors.email.message} />
+    <>
+      <Helmet>
+        <title>Login | Snapsnacks</title>
+      </Helmet>
+      <div className="flex h-screen flex-col items-center">
+        <div className="mt-12 flex w-full max-w-screen-sm flex-col items-center px-5 md:mt-36">
+          <img src={testLogo} alt="test-logo" className="w-20" />
+          <h3 className="mt-8 w-full text-3xl font-semibold">Welcome back!</h3>
+          <p className="text-md mt-4 w-full">
+            Login using your account credentials
+          </p>
+          <form
+            onSubmit={handleSubmit(onSubmitHandler)}
+            className="mt-4 mb-5 flex w-full flex-col"
+          >
+            <div className="mb-4 flex flex-col">
+              <input
+                className="input"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern:
+                    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                })}
+                placeholder="email"
+                type="email"
+                required
+              />
+              {errors.email?.message && (
+                <FormError errorMessage={errors.email.message} />
+              )}
+              {errors.email?.type === "pattern" && (
+                <FormError errorMessage="This email is not valid" />
+              )}
+            </div>
+            <div className="mb-4 flex flex-col">
+              <input
+                className="input"
+                {...register("password", {
+                  required: "Password is required",
+                })}
+                placeholder="password"
+                type="password"
+                required
+              />
+              {errors.password?.message && (
+                <FormError errorMessage={errors.password.message} />
+              )}
+            </div>
+            <Button actionText="Log In" canClick={isValid} loading={loading} />
+            {loginMutationResult?.loginAccount.error && (
+              <FormError
+                errorMessage={loginMutationResult.loginAccount.error}
+              />
             )}
+          </form>
+          <div className="">
+            New Customer?{" "}
+            <Link to="/sign-up" className="link font-semibold">
+              Create Snapsnacks Account!
+            </Link>
           </div>
-          <div className="mb-4 flex flex-col">
-            <input
-              className="input"
-              {...register("password", {
-                required: "Password is required",
-              })}
-              placeholder="password"
-              type="password"
-              required
-            />
-            {errors.password?.message && (
-              <FormError errorMessage={errors.password.message} />
-            )}
-          </div>
-          <Button actionText="Log In" canClick={isValid} loading={loading} />
-          {loginMutationResult?.loginAccount.error && (
-            <FormError errorMessage={loginMutationResult.loginAccount.error} />
-          )}
-        </form>
-        <div className="">
-          New Customer?{" "}
-          <Link to="/sign-up" className="link font-semibold">
-            Create Snapsnacks Account!
-          </Link>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,5 +1,150 @@
-import React from "react";
+import { gql, useMutation } from "@apollo/client";
+import { useForm } from "react-hook-form";
+import { FormError } from "../components/UI/FormError";
+import testLogo from "../images/ruby.svg";
+import { Button } from "../components/UI/Button";
+import { Link, useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
+import { UserRole } from "../api/types/globalTypes";
+import {
+  CreateAccountMutation,
+  CreateAccountMutationVariables,
+} from "../api/types/CreateAccountMutation";
+
+const CREATE_ACCOUNT_MUTATION = gql`
+  mutation CreateAccountMutation($createAccountInput: CreateAccountInput!) {
+    createAccount(input: $createAccountInput) {
+      ok
+      error
+    }
+  }
+`;
+
+interface ISignUpForm {
+  email: string;
+  password: string;
+  role: UserRole;
+}
 
 export default function SignUp() {
-  return <div>signUp</div>;
+  const {
+    register,
+    getValues,
+    formState: { errors, isValid },
+    handleSubmit,
+  } = useForm<ISignUpForm>({
+    mode: "onBlur",
+    defaultValues: { role: UserRole.Client },
+  });
+  const navigate = useNavigate();
+  const onCompleted = (data: CreateAccountMutation) => {
+    const {
+      createAccount: { ok },
+    } = data;
+    if (ok) {
+      navigate("/");
+    }
+  };
+  const [
+    createAccountMutation,
+    { data: createAccountMutationResult, loading },
+  ] = useMutation<CreateAccountMutation, CreateAccountMutationVariables>(
+    CREATE_ACCOUNT_MUTATION,
+    { onCompleted }
+  );
+
+  function onSubmitHandler() {
+    if (loading) return;
+    const { email, password, role } = getValues();
+    createAccountMutation({
+      variables: {
+        createAccountInput: {
+          email,
+          password,
+          role,
+        },
+      },
+    });
+  }
+
+  return (
+    <>
+      <Helmet>
+        <title>SignUp | Snapsnacks</title>
+      </Helmet>
+      <div className="flex h-screen flex-col items-center">
+        <div className="mt-12 flex w-full max-w-screen-sm flex-col items-center px-5 md:mt-36">
+          <img src={testLogo} alt="test-logo" className="w-20" />
+          <h3 className="mt-8 w-full text-3xl font-semibold">Welcome!</h3>
+          <p className="text-md mt-4 w-full">
+            Create your account using credentials
+          </p>
+          <form
+            onSubmit={handleSubmit(onSubmitHandler)}
+            className="mt-4 mb-5 flex w-full flex-col"
+          >
+            <div className="mb-4 flex flex-col">
+              <input
+                className="input"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern:
+                    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                })}
+                placeholder="email"
+                type="email"
+                required
+              />
+              {errors.email?.message && (
+                <FormError errorMessage={errors.email.message} />
+              )}
+              {errors.email?.type === "pattern" && (
+                <FormError errorMessage="This email is not valid" />
+              )}
+            </div>
+            <div className="mb-4 flex flex-col">
+              <input
+                className="input"
+                {...register("password", {
+                  required: "Password is required",
+                })}
+                placeholder="password"
+                type="password"
+                required
+              />
+              {errors.password?.message && (
+                <FormError errorMessage={errors.password.message} />
+              )}
+            </div>
+            <select
+              className="input mb-4"
+              {...register("role", {
+                required: "Role is required",
+              })}
+              required
+            >
+              {Object.keys(UserRole).map((role) => (
+                <option key={role}>{role}</option>
+              ))}
+            </select>
+            {errors.role?.message && (
+              <FormError errorMessage={errors.role.message} />
+            )}
+            <Button actionText="Sign Up" canClick={isValid} loading={loading} />
+            {createAccountMutationResult?.createAccount.error && (
+              <FormError
+                errorMessage={createAccountMutationResult.createAccount.error}
+              />
+            )}
+          </form>
+          <div>
+            Already a Customer?{" "}
+            <Link to="/" className="link font-semibold">
+              Log In Now!
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
