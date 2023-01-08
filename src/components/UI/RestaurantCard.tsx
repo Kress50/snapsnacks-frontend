f7d3d6f3ac42b0fd2714247b3d19dd27576b1eb8
Kress50 <kondalovi@gmail.com>
2023-01-09 00:45:20 +0400

    added RestaurantCard component

---
+++ src/components/UI/RestaurantCard.tsx
@@ -0,0 +1,29 @@
+interface IRestaurantCardProps {
+  name: string;
+  categoryName?: string;
+  image: string | null;
+  id: number;
+}
+
+const RestaurantCard: React.FC<IRestaurantCardProps> = ({
+  name,
+  categoryName,
+  image,
+}) => {
+  return (
+    <div className="group flex cursor-pointer flex-col justify-start transition-all">
+      <div
+        style={{ backgroundImage: `url(${image})` }}
+        className="mb-2 rounded-sm bg-cover bg-center p-20"
+      ></div>
+      <h3 className="pb-1 text-xl font-semibold group-hover:text-amber-500">
+        {name}
+      </h3>
+      <span className="border-t border-gray-300 pt-1 text-sm opacity-50">
+        {categoryName}
+      </span>
+    </div>
+  );
+};
+
+export default RestaurantCard;
