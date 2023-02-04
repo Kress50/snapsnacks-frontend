938d2c631997968972e0e09e47499649256c57e7
Kress50 <kondalovi@gmail.com>
2023-02-04 10:01:35 +0400

    changed profile icon to menu added logout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,23 @@
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { useMeQuery } from "../hooks/useMeQuery";
 import { Logo } from "./UI/Logo";
 
 const Header = () => {
   const { data } = useMeQuery();
+  const [barsHover, setBarsHover] = useState(false);
+
+  const logoutHandler = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
 
   return (
     <>
       {!data?.me.verified && (
-        <div className="fixed z-20 flex w-full -translate-y-7 select-none justify-center bg-amber-500 py-1 text-white transition-transform hover:translate-y-0">
+        <div className="fixed z-50 flex w-full -translate-y-7 select-none justify-center bg-amber-500 py-1 text-white transition-transform hover:translate-y-0">
           <span className="font-bold">Please verify your email</span>
         </div>
       )}
@@ -22,16 +29,32 @@ const Header = () => {
             <span className="text-slate-800">Snacks</span>
           </h1>
         </Link>
-        <Link
-          role="profile-button"
-          className="outline-none focus:text-amber-500"
-          to="/edit-profile"
+        <div
+          onMouseEnter={() => setBarsHover(true)}
+          onMouseLeave={() => setBarsHover(false)}
+          className="relative"
         >
           <FontAwesomeIcon
-            icon={faUser}
+            icon={faBars}
             className="cursor-pointer p-4 text-xl text-slate-800 hover:text-amber-500  active:text-orange-500"
           />
-        </Link>
+          {barsHover && (
+            <div className="absolute right-2 flex cursor-pointer select-none flex-col gap-2 rounded-md border-2 border-amber-500 bg-white p-5 text-center shadow-lg">
+              <Link
+                to="/edit-profile"
+                className="p-1 px-2 hover:text-amber-500 active:text-amber-500"
+              >
+                Profile
+              </Link>
+              <span
+                onClick={logoutHandler}
+                className="border-t p-1 px-2 pt-2 hover:text-amber-500 active:text-amber-500"
+              >
+                Logout
+              </span>
+            </div>
+          )}
+        </div>
       </header>
     </>
   );
